aa7129fb86ba3c2bf0c31c77b8d1b84e3d56dfe8
halsuguro <68799456+halsuguro@users.noreply.github.com>
2020-07-26 12:59:12 +0900

    first

---
+++ app/src/androidTest/java/com/example/empty/calcTest.java
@@ -0,0 +1,17 @@
+package com.example.empty;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class calcTest {
+    private calc mcalc;
+
+    @Test
+    public void plus() {
+        mcalc  = new calc();
+        assertEquals(3, mcalc.plus(2,1),0 );  //Added plus test code
+        assertEquals(2, mcalc.div(2,1),0 );  //Added div test code
+
+    }
+}
---
+++ app/src/main/java/com/example/empty/calc.java
@@ -0,0 +1,17 @@
+package com.example.empty;
+
+public class calc {
+    public int plus(int a, int b) {
+        int c = a + b;
+        return c;
+    }
+    public int div(int a, int b){
+        if (b == 0){
+            return 0;
+        }
+        else {
+            int c = a / b;
+            return c;
+        }
+    }
+}
