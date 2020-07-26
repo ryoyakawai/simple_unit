8bb00c914e6b6f6960ece3b2e8aef7e281bc6670
halsuguro <68799456+halsuguro@users.noreply.github.com>
2020-07-26 19:48:45 +0900

    commit

--- app/src/androidTest/java/com/example/empty/calcTest.java
+++ app/src/androidTest/java/com/example/empty/calcTest.java
@@ -7,8 +7,9 @@ import static org.junit.Assert.*;
 public class calcTest {
     private calc mcalc;
 
-    @Test
+
     public void plus() {
+        @Test
         mcalc  = new calc();
         assertEquals(3, mcalc.plus(2,1),0 );  //Added plus test code
         assertEquals(2, mcalc.div(2,1),0 );  //Added div test code
