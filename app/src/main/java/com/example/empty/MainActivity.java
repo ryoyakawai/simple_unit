c3b60828736ef815ef9735ee7b0780027b69b177
Ryoya Kawai <ryoya.kawai@gmail.com>
2020-07-28 17:35:05 +0900

    add: ui test and enabled jacoco code coverage

--- app/src/main/java/com/example/empty/MainActivity.java
+++ app/src/main/java/com/example/empty/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.empty;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        TextView textView = findViewById(R.id.hello_world);
+        textView.setText(sampleexec(10, 5));
+
     }
+
+    private String sampleexec(int a, int b) {
+        calc cal = new calc();
+
+        int ans00 = cal.div(a, b);
+        int ans01 = cal.plus(a, b);
+
+        Log.d("TEST-DEBUG", Integer.toString(ans00));
+        Log.d("TEST-DEBUG", Integer.toString(ans01));
+
+        return "Hello Android!!";
+    }
+
 }
